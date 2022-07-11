d1265055071c47704f18dbfea7179d70203262b8
hiren100 <hirenramoliya95@gmail.com>
2022-07-11 17:29:23 +0530

    crud javascript code

---
+++ simple_crud_javascript_bootstrap/script.js
@@ -0,0 +1,53 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  formValidation();
+});
+
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be blank";
+    console.log("failure");
+  } else {
+    console.log("successs");
+    msg.innerHTML = "";
+    acceptData();
+  }
+};
+
+let data = {};
+
+let acceptData = () => {
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
+};
+
+let createPost = () => {
+  posts.innerHTML += `
+  <div class="d-flex align-items-center">
+    <p>${data.text}</p>
+    <span class="options">
+    <button type="button" class="btn btn-outline-primary" onClick="editPost(this)">Edit</button>
+    <button type="button" class="btn btn-outline-danger" onClick="deletePost(this)">Delete</button>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
+
+
